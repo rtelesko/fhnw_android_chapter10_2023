6d22be07e783671d817fde81f85de152b3f5d288
Rainer Telesko <rainer.telesko@gmail.com>
2023-04-09 13:09:35 +0200

    Update Firebase CRUD App final

--- FirebaseCRUD/src/main/java/com/example/firebasecrud/MainActivity.java
+++ FirebaseCRUD/src/main/java/com/example/firebasecrud/MainActivity.java
@@ -1,49 +1,77 @@
 package com.example.firebasecrud;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    EditText Name, Last_name, Age, Phone;
-    Button InsertData;
+    Button btSaveScore, btFetchScore;
+    EditText etEnterScore;
     DatabaseReference ref;
-    MemberData member;
+    FirebaseDatabase db;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Name = (EditText) findViewById(R.id.name);
-        Last_name = (EditText) findViewById(R.id.last_name);
-        Age = (EditText) findViewById(R.id.age);
-        Phone = (EditText) findViewById(R.id.phone);
-        InsertData = (Button) findViewById(R.id.btn_insert);
-        ref = FirebaseDatabase.getInstance().getReference().child("Member");
-        member = new MemberData();
+        btSaveScore = findViewById(R.id.btSaveScore);
+        btFetchScore = findViewById(R.id.btFetchScore);
+        etEnterScore = findViewById(R.id.etEnterScore);
+    }
 
-        InsertData.setOnClickListener(new View.OnClickListener() {
+    protected void onStart() {
+        super.onStart();
+        // Fetch Firebase reference
+        db = FirebaseDatabase.getInstance();
+        ref = db.getReference("score");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        btSaveScore.setOnClickListener(view -> writeToFirebase());
+        btFetchScore.setOnClickListener(view -> readFromFirebase());
+    }
+
+    // Storing the score in Firebase
+    protected void writeToFirebase() {
+        score = Integer.parseInt(etEnterScore.getText().toString());
+        Toast.makeText(MainActivity.this, "You entered: " + score, Toast.LENGTH_SHORT).show();
+        ref.setValue(score).addOnFailureListener(e -> {
+            // Write to Firebase failed
+            Toast.makeText(MainActivity.this, "Write to Firebase failed!", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    // Reading the score from Firebase
+    protected void readFromFirebase() {
+        // Read from the database
+        ref.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                int M_age = Integer.parseInt(Age.getText().toString().trim());
-                Long M_phone = Long.parseLong(Phone.getText().toString().trim());
-                member.setName(Name.getText().toString().trim());
-                member.setLast_name(Last_name.getText().toString().trim());
-                member.setAge(M_age);
-                member.setPhone(M_phone);
-                ref.push().setValue(member);
-                Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                int score = dataSnapshot.getValue(Integer.class);
+                Toast.makeText(MainActivity.this, "Value is: " + score, Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(MainActivity.this, "Failed to read value!" + score, Toast.LENGTH_SHORT).show();
+                Log.w("MainActivity", "Failed to read value!", error.toException());
             }
         });
-
-
     }
 }
